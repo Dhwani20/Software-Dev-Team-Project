e39d74cbbfaa20cbfcd77524e5e7b1dcb6a1e6cc
Dhwani20 <Dhwani20@users.noreply.github.com>
2017-03-29 22:08:47 -0600

    map-script updated

---
+++ World.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World : MonoBehaviour
+{
+    //https:/maps.googleapis.com/maps/api/geocode/json?latlng=55.4018502,10.3419367&zoom=18&size=512x512&scale=2&maptype=roadmap&format=xml;
+
+    public double latitude = 55.4018502;
+    public double longtitude = 10.3419367;
+
+    public Grid worldGrid;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        worldGrid = new Grid(3, 40, latitude, longtitude);
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (worldGrid.needsUpdate)
+        {
+            List<Grid.GridItem> itemsToRender = worldGrid.GetRenderQueue();
+            for (int index = 0; index < itemsToRender.Count; index++)
+            {
+                //Debug.Log ("Rendering " + itemsToRender [index].getImageUrl());
+                StartCoroutine(itemsToRender[index].Download());
+            }
+            worldGrid.needsUpdate = false;
+        }
+    }
+}
