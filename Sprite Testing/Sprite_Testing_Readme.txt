edad96d4593a70b4ea4c149b7f82339999928cf3
zaha0077 <zaha0077@colorado.edu>
2017-04-03 13:30:55 -0600

    Add files via upload
    Contains a script to test sprites and a scene to implement the script in.

---
+++ Sprite Testing/Sprite_Testing_Readme.txt
@@ -0,0 +1,15 @@
+Instructions:
+Place the contents of this folder into a unity Assets folder. Do NOT drag the folder itself in.
+
+---------------------
+The script, when attached to an object, (The provided scene contains such an object)
+will function as follows:
+
+When the game is running, press any of the numbers 1-6* as inputs and the script will have the object switches to the corresponding sprite 
+
+Pressing an invalid key or an out of range number will display an error sprite.
+
+It's simple, but it allows us to see that the sprites are indeed being loaded correctly.
+
+---------------------
+*Note that 1-6 refers to the alphanumeric 1-6 keys (the ones at the top of the keyboard), not the numpad
---
+++ Sprite Testing/TestingSprites.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingSprites : MonoBehaviour {
+    int spriteIndex = 1;
+
+    Sprite currentSprite; 
+
+    void Start () {
+        currentSprite = Resources.Load<Sprite>("01");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<SpriteRenderer>().sprite = currentSprite;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))//checks for valid keys.
+        {
+            spriteIndex = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            spriteIndex = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            spriteIndex = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            spriteIndex = 4;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            spriteIndex = 5;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            spriteIndex = 6;
+        }
+        else if (Input.anyKeyDown && !(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)))  //sends error for invalid keys.
+        {
+            spriteIndex = 0;
+        }
+
+        UpdateSprite();
+
+    }
+
+    void UpdateSprite() {
+        switch (spriteIndex)
+        { //handles sprite changes
+            case 1:
+                currentSprite = Resources.Load<Sprite>("01");
+                break;
+            case 2:
+                currentSprite = Resources.Load<Sprite>("02");
+                break;
+            case 3:
+                currentSprite = Resources.Load<Sprite>("03");
+                break;
+            case 4:
+                currentSprite = Resources.Load<Sprite>("04A");
+                break;
+            case 5:
+                currentSprite = Resources.Load<Sprite>("04B");
+                break;
+            case 6:
+                currentSprite = Resources.Load<Sprite>("05");
+                break;
+            default:
+                currentSprite = Resources.Load<Sprite>("ERROR"); //This sprite should not be seen unless there is an error.
+                break;
+
+
+        }
+    }
+}
