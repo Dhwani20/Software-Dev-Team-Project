8aa93995a44b62aa7d0f989a651e516fd2531347
Jo Hendrix <jrhendrix@engr2-21-245-dhcp.int.colorado.edu>
2017-04-05 17:47:44 -0600

    game update

---
+++ Hendrix/Menu/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	//public GameObject loadingImage;
+	public int nextLevel;
+
+	//public void LoadScene(int level)
+	public void LoadScene()
+	{
+		//loadingImage.SetActive(true);
+		//Application.LoadLevel(level);
+		Application.LoadLevel(nextLevel);
+	}
+}
