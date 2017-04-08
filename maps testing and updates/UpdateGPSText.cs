a7188d9a8f5ee0247e7d12ce1f6223c3788d173c
Dhwani20 <Dhwani20@users.noreply.github.com>
2017-04-07 18:14:05 -0600

    Create UpdateGPSText.cs

---
+++ maps testing and updates/UpdateGPSText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour
+{
+    public Text coordinates;
+
+    private void Update()
+    {
+        coordinates.text = "Lat:" + GPS.Instance.ToString() + "Lon" + GPS.Instance.longitude.ToString();
+    }
+
+
+}
